633ef0da37eecffdf333e916cad433ce5360bd2f
Tobias Rose <trose@neuro.mpg.de>
2019-01-15 08:07:39 +0100

    comments

--- lowpassfilt.m
+++ lowpassfilt.m
@@ -1,6 +1,9 @@
 function filtered = lowpassfilt(signal, order, fc, fs, type)
-
-%TR2019 switchboard to standard ML 'analog' filters.
+%LOWPASSFILT  Switchboard for ML lowwpass bessel and butterworth filters
+%   filtered = LOWPASSFILT(signal, order, fc, fs, type) filters the input
+%   signal at a cutoff freqeuency (fc, in Hz) of sampling rate (fs) using either a Bessel (type = 'Bessel') or
+%   Butterworth (type = 'Butter') filter of 'order' order (aka pole). 
+   
 
 if nargin < 5
     type = 'Bessel';
@@ -10,9 +13,9 @@ if type == 'Butter'
     [b,a] = butter(order,fc/(fs/2), 'low');
 elseif type == 'Bessel'
     [b,a] = besself(order,fc * 2 * pi(), 'low');
-    [b,a] = bilinear(b,a,fs); %digital to analog conversion (besself is analog only)
+    [b,a] = bilinear(b,a,fs);                       % digital to analog conversion (besself is analog only)
 end
 
-filtered = filtfilt(b,a,signal); % bidirectional filtering w/o phase distortion
+filtered = filtfilt(b,a,signal);                    % bidirectional filtering w/o phase distortion
 
   
